9d3e1d707e0a422849066a9e848e4f6b21bd560b
root <root@serverproduction.onprocess.work>
2022-06-29 10:55:54 +0800

    update title

--- resources/views/front-end/layouts/master.blade.php
+++ resources/views/front-end/layouts/master.blade.php
@@ -6,9 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-    <title>@yield('title',"Sistem Informasi Pertanian")</title>
+    <title>@yield('title',env("APP_NAME"))</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="@yield('title',"Sistem Informasi Pertanian")">
+    <meta name="description" content="@yield('title',env("APP_NAME"))">
     <style>
         .error{
             color: red;
